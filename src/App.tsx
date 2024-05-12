7c583312d0355a7a12226f32f0930acfb5a5de27
dreamqip <v.chuchupak@gmail.com>
2024-05-12 20:44:16 +0200

    feat: add TWA integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,91 @@
+import { fromNano } from '@ton/core';
 import { TonConnectButton } from '@tonconnect/ui-react';
+import WebApp from '@twa-dev/sdk';
 import './App.css';
-import reactLogo from './assets/react.svg';
 import { useMainContract } from './hooks/use-main-contract';
-import viteLogo from '/vite.svg';
+import { useTonConnect } from './hooks/use-ton-connect';
 
 function App() {
-    const { contractAddress, contractBalance, counter, latestAddress, ownerAddress, increment } = useMainContract();
+  const {
+    contractAddress,
+    balance,
+    counter,
+    increment,
+    deposit,
+    withdraw,
+  } = useMainContract();
 
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <button onClick={() => increment()}>Increment</button>
+  const { connected } = useTonConnect();
 
-            <div className="card">
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-            <TonConnectButton />
-            <div>
-                <div className="Card">
-                    <b>Our contract Address</b>
-                    <div className="Hint">{contractAddress?.slice(0, 30) + '...'}</div>
-                    <b>Our contract Balance</b>
-                    <div className="Hint">{contractBalance?.toString()}</div>
-                </div>
+  const showAlert = () => {
+    WebApp.showAlert('Hello from React');
+  };
 
-                <div className="Card">
-                    <b>Counter Value</b>
-                    <div>{counter ?? 'Loading...'}</div>
-                </div>
+  return (
+    <div>
+      <div>
+        <TonConnectButton />
+      </div>
+      <div>
+        <div className="Card">
+          <div>TWA Platform {<b>{WebApp.platform}</b>}</div>
+          <b>Our contract Address</b>
+          <div className="">{contractAddress?.slice(0, 30) + '...'}</div>
+          <b>Our contract Balance</b>
+          <span>
+            {balance?.toString() && (
+              <div className="">{fromNano(balance)}</div>
+            )}
+          </span>
+        </div>
 
-                <div className="Card">
-                    <b>Latest Address</b>
-                    <div>{latestAddress ?? 'Loading...'}</div>
-                </div>
+        <div className="Card">
+          <b>Counter Value</b>
+          <div>{counter ?? 'Loading...'}</div>
+        </div>
 
-                <div className="Card">
-                    <b>Owner Address</b>
-                    <div>{ownerAddress ?? 'Loading...'}</div>
-                </div>
-            </div>
-        </>
-    );
+        <div className="button-group">
+          <button
+            onClick={() => {
+              showAlert();
+            }}
+          >
+            Show Alert
+          </button>
+
+          {connected && (
+            <button
+              onClick={() => {
+                increment();
+              }}
+            >
+              Increment by 1
+            </button>
+          )}
+
+          {connected && (
+            <button
+              onClick={() => {
+                deposit(1);
+              }}
+            >
+              Request deposit of 1 TON
+            </button>
+          )}
+
+          {connected && (
+            <button
+              onClick={() => {
+                withdraw(0.7);
+              }}
+            >
+              Request 0.7 TON withdrawal
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/use-main-contract.ts
+++ src/hooks/use-main-contract.ts
@@ -6,63 +6,84 @@ import { useTonClient } from './use-ton-client';
 import { useTonConnect } from './use-ton-connect';
 
 type ContractData = {
-    counter: number;
-    latestAddress: string;
-    ownerAddress: string;
+  counter: number;
+  balance: bigint;
+  latestAddress: string;
+  ownerAddress: string;
 };
 
 async function sleep(ms: number) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export function useMainContract() {
-    const [contractData, setContractData] = React.useState<ContractData | null>(null);
+  const [contractData, setContractData] = React.useState<ContractData | null>(
+    null
+  );
 
-    const client = useTonClient();
-    const { sender } = useTonConnect();
+  const client = useTonClient();
+  const { sender } = useTonConnect();
 
-    const contract = useAsync(async () => {
-        if (!client) return;
+  const contract = useAsync(async () => {
+    if (!client) return;
 
-        const contract = new Main(address('EQAf9NsFLC4BOS8n0b1PPvUNzJa8yDMvidyfVCHsqF54RMKp'));
+    const contract = new Main(
+      address('EQAf9NsFLC4BOS8n0b1PPvUNzJa8yDMvidyfVCHsqF54RMKp')
+    );
 
-        return client.open(contract);
-    }, [client]);
+    return client.open(contract);
+  }, [client]);
 
-    const contractBalance = useAsync(async () => {
-        if (!contract) return;
+  const contractBalance = useAsync(async () => {
+    if (!contract) return;
 
-        return contract.getContractBalance();
-    });
+    return contract.getContractBalance();
+  }, [contract]);
 
-    React.useEffect(() => {
-        async function getValue() {
-            if (!contract) return;
+  React.useEffect(() => {
+    async function getValue() {
+      if (!contract) return;
 
-            setContractData(null);
+      setContractData(null);
 
-            const { counter, latestAddress, ownerAddress } = await contract.getContractData();
-            setContractData({
-                counter,
-                latestAddress: latestAddress.toString(),
-                ownerAddress: ownerAddress.toString(),
-            });
+      const { counter, latestAddress, ownerAddress } =
+        await contract.getContractData();
+      const balance = contractBalance;
+      setContractData({
+        counter,
+        latestAddress: latestAddress.toString(),
+        ownerAddress: ownerAddress.toString(),
+        balance: balance ?? BigInt(0),
+      });
 
-            await sleep(5000);
-            getValue();
-        }
+      await sleep(5000);
+      getValue();
+    }
 
-        getValue();
-    }, [contract]);
+    getValue();
+  }, [contract, contractBalance]);
 
-    const increment = () => {
-        return contract?.sendIncrement(sender, toNano('0.05'));
-    };
+  const increment = () => {
+    return contract?.sendIncrement(sender, toNano(String(0.009)));
+  };
 
-    return {
-        contractAddress: contract?.address.toString(),
-        contractBalance,
-        increment,
-        ...contractData,
-    };
+  const deposit = (amount: number) => {
+    return contract?.sendDeposit(sender, toNano(amount));
+  };
+
+  const withdraw = (amount: number) => {
+    return contract?.sendWithdrawalRequest(
+      sender,
+      toNano(String(0.009)),
+      toNano(amount)
+    );
+  };
+
+  return {
+    contractAddress: contract?.address.toString(),
+    increment,
+    deposit,
+    withdraw,
+    ...contractData,
+  };
 }
